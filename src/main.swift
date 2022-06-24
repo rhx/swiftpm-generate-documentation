bba24e6046ecdde04327027fb0bd5c31d0c98722
Callum McColl <callum.mccoll1@gmail.com>
2022-06-24 14:04:19 +1000

    Moved all functionality into methods

--- src/main.swift
+++ src/main.swift
@@ -4,6 +4,59 @@ struct Package: Hashable, Codable {
 
     var targets: [Target]
 
+    init(dumpPackageUsing swiftBin: String = "/usr/bin/swift") throws {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: swiftBin, isDirectory: false)
+        process.arguments = ["package", "dump-package"]
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        print(([process.executableURL?.path  ?? ""] + (process.arguments ?? [])).joined(separator: " "))
+        try process.run()
+        process.waitUntilExit()
+        guard process.terminationStatus == 0 else {
+            fatalError("Unable to dump package")
+        }
+        guard let data = try pipe.fileHandleForReading.readToEnd() else {
+            fatalError("Failed to read package dump")
+        }
+        let decoder = JSONDecoder()
+        var package = try decoder.decode(Package.self, from: data)
+        let fm = FileManager.default
+        let srcDir = URL(fileURLWithPath: fm.currentDirectoryPath, isDirectory: true).appendingPathComponent("Sources", isDirectory: true)
+        package.targets = package.targets.compactMap {
+            if $0.type == .unsupported {
+                return nil
+            }
+            if $0.type != .regular {
+                return $0
+            }
+            let targetDir = srcDir.appendingPathComponent($0.name, isDirectory: true)
+            guard let enumerator = fm.enumerator(at: targetDir, includingPropertiesForKeys: nil) else {
+                return nil
+            }
+            let files: [URL] = enumerator.allObjects.compactMap {
+                guard let url = $0 as? URL, !url.hasDirectoryPath else {
+                    return nil
+                }
+                return url
+            }
+            if files.contains(where: { $0.pathExtension == "swift" }) {
+                return Target(name: $0.name, type: .swift)
+            } else if files.contains(where: { $0.pathExtension == "c" || $0.pathExtension == "h" || $0.pathExtension == "hpp" || $0.pathExtension == "cc" || $0.pathExtension == "cpp" }) {
+                return Target(name: $0.name, type: .C)
+            } else {
+                return nil
+            }
+        }
+        self = package
+    }
+
+    func generateDocumentation(swiftBin: String = "/usr/bin/swift", hostingBasePath: String?, outputPath: String = "./docs") throws {
+        for target in targets where target.type == .swift || target.type == .C {
+            try target.generateDocumentation(swiftBin: swiftBin, hostingBasePath: hostingBasePath, outputPath: outputPath)
+        }
+    }
+
 }
 
 struct Target: Hashable, Codable {
@@ -40,6 +93,33 @@ struct Target: Hashable, Codable {
     var name: String
 
     var type: TargetType
+
+    func generateDocumentation(swiftBin: String = "/usr/bin/swift", hostingBasePath: String?, outputPath: String = "./docs") throws {
+        switch type {
+        case .swift:
+            try generateSwiftDocumentation(swiftBin: swiftBin, hostingBasePath: hostingBasePath, outputPath: outputPath)
+        default:
+            return
+        }
+    }
+    
+
+    private func generateSwiftDocumentation(swiftBin: String, hostingBasePath: String?, outputPath: String) throws {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: swiftBin, isDirectory: false)
+        process.arguments = ["package", "--allow-writing-to-directory", outputPath, "generate-documentation", "--target", self.name, "--disable-indexing", "--transform-for-static-hosting"] + (hostingBasePath != nil ? ["--hosting-base-path", hostingBasePath!] : []) + ["--output-path", outputPath]
+        print(([process.executableURL?.path  ?? ""] + (process.arguments ?? [])).joined(separator: " "))
+        do {
+            try process.run()
+        } catch let e {
+            fatalError(e.localizedDescription)
+        }
+        process.waitUntilExit()
+        guard process.terminationStatus == 0 else {
+            fatalError("Unable to generate documentation for target \(self.name)")
+        }
+    }
+
 }
 
 
@@ -48,72 +128,9 @@ print(swiftBin)
 let outputPath = ProcessInfo.processInfo.environment["INPUT_OUTPUT_PATH"] ?? "./docs"
 let hostingBasePath = ProcessInfo.processInfo.environment["INPUT_HOSTING_BASE_PATH"]
 
-let targets: [Target] = {
-    do {
-        let process = Process()
-        process.executableURL = URL(fileURLWithPath: "/usr/bin/swift", isDirectory: false)
-        process.arguments = ["package", "dump-package"]
-        let pipe = Pipe()
-        process.standardOutput = pipe
-        print(([process.executableURL?.path  ?? ""] + (process.arguments ?? [])).joined(separator: " "))
-        try process.run()
-        process.waitUntilExit()
-        guard process.terminationStatus == 0 else {
-            fatalError("Unable to dump package")
-        }
-        guard let data = try pipe.fileHandleForReading.readToEnd() else {
-            fatalError("Failed to read package dump")
-        }
-        let decoder = JSONDecoder()
-        let package = try decoder.decode(Package.self, from: data)
-        let fm = FileManager.default
-        let srcDir = URL(fileURLWithPath: fm.currentDirectoryPath, isDirectory: true).appendingPathComponent("Sources", isDirectory: true)
-        return package.targets.compactMap {
-            if $0.type == .unsupported {
-                return nil
-            }
-            if $0.type != .regular {
-                return $0
-            }
-            let targetDir = srcDir.appendingPathComponent($0.name, isDirectory: true)
-            guard let enumerator = fm.enumerator(at: targetDir, includingPropertiesForKeys: nil) else {
-                return nil
-            }
-            let files: [URL] = enumerator.allObjects.compactMap {
-                guard let url = $0 as? URL, !url.hasDirectoryPath else {
-                    return nil
-                }
-                return url
-            }
-            if files.contains(where: { $0.pathExtension == "swift" }) {
-                return Target(name: $0.name, type: .swift)
-            } else if files.contains(where: { $0.pathExtension == "c" || $0.pathExtension == "h" || $0.pathExtension == "hpp" || $0.pathExtension == "cc" || $0.pathExtension == "cpp" }) {
-                return Target(name: $0.name, type: .C)
-            } else {
-                return nil
-            }
-        }
-    } catch let e {
-        fatalError(e.localizedDescription)
-    }
-}()
-
-func generateDocCDocumentation(for target: Target, swiftBin: String, hostingBasePath: String?, outputPath: String) {
-    let process = Process()
-    process.executableURL = URL(fileURLWithPath: swiftBin, isDirectory: false)
-    process.arguments = ["package", "--allow-writing-to-directory", outputPath, "generate-documentation", "--target", target.name, "--disable-indexing", "--transform-for-static-hosting"] + (hostingBasePath != nil ? ["--hosting-base-path", hostingBasePath!] : []) + ["--output-path", outputPath]
-    print(([process.executableURL?.path  ?? ""] + (process.arguments ?? [])).joined(separator: " "))
-    do {
-        try process.run()
-    } catch let e {
-        fatalError(e.localizedDescription)
-    }
-    process.waitUntilExit()
-    guard process.terminationStatus == 0 else {
-        fatalError("Unable to generate documentation for target \(target.name)")
-    }
-}
-
-for target in targets where target.type == .swift {
-    generateDocCDocumentation(for: target, swiftBin: swiftBin, hostingBasePath: hostingBasePath, outputPath: outputPath)
+do {
+    let package = try Package(dumpPackageUsing: swiftBin)
+    try package.generateDocumentation(swiftBin: swiftBin, hostingBasePath: hostingBasePath, outputPath: outputPath)
+} catch let e {
+    fatalError(e.localizedDescription)
 }
